887b4e4d3e254b52c6880f723bc43d6cc0b79690
Atsukoro1 <dornicakkuba@gmail.com>
2022-09-04 16:59:01 +0200

    Fixed navbar and progressline

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-900">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
+        <div className="fixed flex h-16 items-center">
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
               <h1 className="text-2xl font-bold">
@@ -17,7 +17,7 @@ const Navbar = () => {
               </h1>
             </div>
           </div>
-          <div className="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <div className="fixed right-0 flex items-center mr-8">
             <DarkModeButton/>
 
             <motion.div 
